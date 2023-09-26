aade22aae6426332dac75eb512e72aec7483af44
shang yang <yangshang910215@gmail.com>
2023-09-25 22:20:27 -0400

    svelte-demo

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Svelte-demo
+
+https://bugfender.com/blog/create-your-first-svelte-app/
+npx degit sveltejs/template svelte-hobbies
+cd svelte-hobbies
+npm install
+npm run dev
---
+++ svelte-demo/src/App.svelte
@@ -0,0 +1,65 @@
+<script>
+  let newItem = '';
+  let items = [];
+
+  function addItem() {
+    if (newItem.trim() !== '') {
+      items = [...items, newItem];
+      newItem = '';
+    }
+  }
+</script>
+<main>
+<h1>Svelte Demo App</h1>
+
+<input bind:value={newItem} placeholder="Enter an item..." />
+
+<button on:click={addItem}>Add</button>
+
+<ul>
+  {#each items as item (item)}
+    <li>{item}</li>
+  {/each}
+</ul>
+</main>
+
+
+<style>
+  h1 {
+    text-align: center;
+    margin-bottom: 20px;
+    color: #333;
+  }
+
+  input {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+
+  button {
+    display: block;
+    width: 100%;
+    padding: 10px;
+    background-color: #007acc;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #005ea6;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    background-color: #f2f2f2;
+    padding: 10px;
+    margin-bottom: 5px;
+    border-radius: 5px;
+  }
+</style>
